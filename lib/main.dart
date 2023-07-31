4e36d7f96e545985a878ce04f0e9ceba139466d3
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-08-01 01:14:24 +0530

    firebase connect

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:chat_messenger_app/pages/login.dart';
 import 'package:chat_messenger_app/pages/register_page.dart';
 import 'package:chat_messenger_app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
